05d60ed1bf04d8c59e836095d7e6d018711233fe
aditya <adityatandulkar20@gmail.com>
2023-03-03 18:49:34 +0530

    initial commit

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,12 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+    const [state, setState] = useState({[stateVar] : value});
+    function update(val){
+        setState({'key' : val});
+    }
+
+  return [state, update];
 };
 
 export default useLocalStorage;
